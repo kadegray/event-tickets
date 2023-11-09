deb13b569502889558710d17b26cd6b68eeb67a9
Kade Gray <kadegray@gmail.com>
2023-11-09 10:36:44 +1000

    EventsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'events', component: EventsComponent },
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
